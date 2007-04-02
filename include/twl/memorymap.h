26b07d9d71217a67b215e762df6697d5d48897c9
nakasima <nakasima@4ee2a332-4b2b-5046-8439-1ba90f034370>
2007-04-02 05:15:10 +0000

    add ioreg build.
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/twl_wrapsdk/trunk@3 4ee2a332-4b2b-5046-8439-1ba90f034370

---
+++ include/twl/memorymap.h
@@ -0,0 +1,43 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - include - HW
+  File:     memorymap.h
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Log: $
+  $NoKeywords: $
+ *---------------------------------------------------------------------------*/
+#ifndef TWL_MEMORYMAP_H_
+#define TWL_MEMORYMAP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef	SDK_ARM9
+#include	<nitro/hw/ARM9/mmap_global.h>
+#include	<nitro/hw/ARM9/mmap_main.h>
+#include	<nitro/hw/ARM9/mmap_tcm.h>
+#include	<nitro/hw/ARM9/mmap_vram.h>
+#include	<nitro/hw/common/mmap_shared.h>
+#include	<twl/hw/ARM9/ioreg.h>
+
+#else  //SDK_ARM7
+#include	<nitro/hw/ARM7/mmap_global.h>
+#include	<nitro/hw/ARM7/mmap_main.h>
+#include	<nitro/hw/ARM7/mmap_wram.h>
+#include	<nitro/hw/common/mmap_shared.h>
+#include	<twl/hw/ARM7/ioreg.h>
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+/* TWL_MEMORYMAP_H_ */
+#endif
---
+++ include/twl/memorymap_sp.h
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------*
+  Project:  TwlSDK - include
+  File:     memorymap_sp.h
+
+  Copyright 2007 Nintendo.  All rights reserved.
+
+  These coded instructions, statements, and computer programs contain
+  proprietary information of Nintendo of America Inc. and/or Nintendo
+  Company Ltd., and are protected by Federal copyright law.  They may
+  not be disclosed to third parties or copied or duplicated in any form,
+  in whole or in part, without the prior written consent of Nintendo.
+
+  $Log: $
+  $NoKeywords: $
+ *---------------------------------------------------------------------------*/
+#ifndef	SDK_ARM7
+#define	SDK_ARM7
+#endif
+#ifdef	SDK_ARM9
+#undef	SDK_ARM9
+#endif
+#include <twl/memorymap.h>
