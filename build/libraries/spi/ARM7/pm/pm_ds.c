850c23ba52a81165d9140b47115765650c38f1a5
nakasima <nakasima@4ee2a332-4b2b-5046-8439-1ba90f034370>
2007-10-10 02:20:36 +0000

    adjust PMIC LED registers to MCU.
    git-svn-id: file:///Users/lillianskinner/Downloads/platinum/twl/twl_wrapsdk/trunk@297 4ee2a332-4b2b-5046-8439-1ba90f034370

--- build/libraries/spi/ARM7/pm/pm_ds.c
+++ build/libraries/spi/ARM7/pm/pm_ds.c
@@ -34,19 +34,19 @@ void PM_GoDsMode( void )
     // LED
     PMi_ResetFlags( REG_PMIC_LED_CTL_ADDR, PMIC_LED_CTL_L12_AT_BLK | PMIC_LED_CTL_L12_BLK_BY_SLP );
     PMi_SetParams( REG_PMIC_LED12_B4_ADDR, 
-                   PMIC_LED12_B4_L1_OFF  | PMIC_LED12_B4_L2_OFF,
+                   PMIC_LED12_B4_L1_LOW  | PMIC_LED12_B4_L2_LOW,
                    PMIC_LED12_B4_L1_MASK | PMIC_LED12_B4_L2_MASK 
                  );
     PMi_SetParams( REG_PMIC_LED12_B3_ADDR, 
-                   PMIC_LED12_B3_L1_100  | PMIC_LED12_B3_L2_OFF,
+                   PMIC_LED12_B3_L1_HIGH | PMIC_LED12_B3_L2_LOW,
                    PMIC_LED12_B3_L1_MASK | PMIC_LED12_B3_L2_MASK 
                  );
     PMi_SetParams( REG_PMIC_LED12_B2_ADDR, 
-                   PMIC_LED12_B2_L1_OFF  | PMIC_LED12_B2_L2_100,
+                   PMIC_LED12_B2_L1_LOW  | PMIC_LED12_B2_L2_HIGH,
                    PMIC_LED12_B2_L1_MASK | PMIC_LED12_B2_L2_MASK 
                  );
     PMi_SetParams( REG_PMIC_LED12_B1_ADDR, 
-                   PMIC_LED12_B1_L1_100  | PMIC_LED12_B1_L2_100,
+                   PMIC_LED12_B1_L1_HIGH | PMIC_LED12_B1_L2_HIGH,
                    PMIC_LED12_B1_L1_MASK | PMIC_LED12_B1_L2_MASK 
                  );
 
@@ -54,8 +54,8 @@ void PM_GoDsMode( void )
     PMi_SetFlags( REG_PMIC_CTL2_ADDR, PMIC_CTL2_LCD_PWR );
 
     // back light ON
-    PMi_SetParams( REG_PMIC_BL1_BRT_ADDR, 0x16 << PMIC_BL1_BRT_SHIFT, PMIC_BL1_BRT_MASK );
-    PMi_SetParams( REG_PMIC_BL2_BRT_ADDR, 0x16 << PMIC_BL2_BRT_SHIFT, PMIC_BL2_BRT_MASK );
+    PMi_SetParams( REG_PMIC_BL1_BRT_ADDR, PMIC_BL_BRT_DEFAULT, PMIC_BL1_BRT_MASK );
+    PMi_SetParams( REG_PMIC_BL2_BRT_ADDR, PMIC_BL_BRT_DEFAULT, PMIC_BL2_BRT_MASK );
     OS_SpinWait( OS_MSEC_TO_CPUCYC( 17*2 ) );
     PMi_SetFlags( REG_PMIC_CTL2_ADDR, PMIC_CTL2_BKLT1 | PMIC_CTL2_BKLT2 );
 
